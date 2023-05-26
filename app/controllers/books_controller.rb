7badbe79e314a5a774b98d523bae47c746ae3c6b
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-05-26 07:02:33 +0000

    [Stop3]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.new
+    @books = Book.all
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.new
+    @books = @user.books
   end
 
   def edit
